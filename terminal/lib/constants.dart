96d86aebc27c019b2910c45fea652e1af861ac72
Thomas Juen <office@soumasoft.com>
2024-09-21 00:14:02 +0200

    cleanup

--- terminal/lib/constants.dart
+++ terminal/lib/constants.dart
@@ -1,5 +1,4 @@
 const kAssetMflLogoSlim = 'assets/images/mfl_logo_slim.png';
-const kAssetRfidIconSlim = 'assets/images/rfid_icon_slim.png';
 
 const kLocalStorageKeySettings = 'settings';
 const kLocalStorageKeySelectedTerminalEndpoint = 'selectedTerminalEndpoint';
