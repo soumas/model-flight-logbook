ceee6de5eabcebb6d52c76cce15b09e793e31301
Thomas Juen <office@soumasoft.com>
2025-02-07 19:06:08 +0100

    go

--- terminal/lib/domain/enums/utm_sync_status.dart
+++ terminal/lib/domain/enums/utm_sync_status.dart
@@ -5,10 +5,10 @@ part 'utm_sync_status.mapper.dart';
 
 @MappableEnum()
 enum UtmSyncStatus {
-  unknown(icon: Icons.hourglass_bottom, color: Colors.orange, label: 'Status unbekannt - bitte warten'),
+  unknown(icon: Icons.hourglass_bottom, color: Colors.orange, label: 'Status unbekannt'),
   noActiveFlight(icon: Icons.warning, color: Colors.red, label: 'Keine Flugfreigabe'),
   flying(icon: Icons.airplanemode_active, color: Colors.green, label: 'Flugfreigabe vorhanden'),
-  error(icon: Icons.hourglass_bottom, color: Colors.orange, label: 'Status unbekannt - bitte warten'),
+  error(icon: Icons.hourglass_bottom, color: Colors.orange, label: 'Status unbekannt'),
   updateFailed(icon: Icons.error, color: Colors.red, label: 'Statusupdate fehlgeschlagen');
 
   final IconData icon;
