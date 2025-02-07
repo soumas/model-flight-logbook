ceac3f577208a9ee39a16b219d39077040797def
Thomas Juen <office@soumasoft.com>
2025-02-07 22:51:02 +0100

    try to enable ssl

--- server/api/apimanager.py
+++ server/api/apimanager.py
@@ -1,6 +1,5 @@
 
 from contextlib import asynccontextmanager
-import ssl
 from fastapi import FastAPI
 from fastapi.security import APIKeyHeader
 from utils.scheduler import scheduler
@@ -23,6 +22,3 @@ async def lifespan(app: FastAPI):
 
 api = FastAPI(debug=config.logbook.debug, lifespan=lifespan)
 api_key_header = APIKeyHeader(name="x-api-key")
-
-ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-ssl_context.load_cert_chain('/home/pi/model-flight-logbook/cert.pem', keyfile='/home/pi/model-flight-logbook/key.pem')
