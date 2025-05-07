9e81c132b36d1180c935ac908d231318a243b46c
Thomas Juen <office@soumasoft.com>
2025-05-08 00:06:31 +0200

    mfl-admin release

---
+++ server/mfl-admin/assets/Logout-BLmwXsa0.js
@@ -0,0 +1,1 @@
+import{A as o}from"./AuthService-BBAtoNmS.js";import{o as e,c as t}from"./index-Bq2cTg_4.js";const m={__name:"Logout",setup(r){return o.logout(),(c,a)=>(e(),t("div"))}};export{m as default};
--- server/mfl-admin/assets/Logout-BSDj-h7n.js
+++
@@ -1,1 +0,0 @@
-import{A as o}from"./AuthService-C9kJo9V5.js";import{o as e,c as t}from"./index-C8DqRj8t.js";const m={__name:"Logout",setup(r){return o.logout(),(c,a)=>(e(),t("div"))}};export{m as default};
